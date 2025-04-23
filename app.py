7a2d6c5cb08b2c57ac4e77b63db9915184caccf5
ghaway <ghaway@hotmail.com>
2025-04-23 12:08:11 +0200

    corrections diverses

--- app.py
+++ app.py
@@ -56,5 +56,6 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
